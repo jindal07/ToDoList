0da08f1d6f480f5bd37a36f2809135f98ec400e1
Harsh Jindal <121694647+jindal07@users.noreply.github.com>
2025-06-01 20:13:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -132,7 +132,7 @@ const App = () => {
   return (
     <div className="max-w-2xl mx-auto my-10 p-6 bg-gray-200 rounded-lg shadow-lg">
       <div>
-        <img className=' mx-auto my-auto px-1 py-2' src='./src/assets/clipboard-check.png'></img>
+        <img className=' mx-auto my-auto px-1 py-2' src='./src/assets/clipboard-check.png'>
         <h1 className="text-3xl font-bold text-gray-800 mb-6 text-center">
           Get It Done
         </h1>
